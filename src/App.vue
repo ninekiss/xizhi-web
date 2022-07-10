ae0f8db3e6601cc49bd2235b4671c53de3daac2c
lankeo <aodocoo@gmail.com>
2022-07-10 23:03:58 +0800

    Inital commit.

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <RouterView />
+
+  <footer class="footer">
+    <div class="beian">
+      <a href="https://beian.miit.gov.cn/" target="_blank"
+        >陕ICP备20011829号-1</a
+      >
+      <a
+        href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=61019002001182"
+        target="_blank"
+        >陕公安网备61019002001182号</a
+      >
+    </div>
+  </footer>
+</template>
+
+<style>
+body {
+  margin: 0;
+  overflow-x: hidden;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  min-width: 375px;
+  width: 100%;
+  background: #040d21;
+}
+
+.footer {
+  height: 2rem;
+}
+
+.beian {
+  display: flex;
+  align-items: center;
+  gap: 20px;
+  height: 2rem;
+}
+
+.beian a {
+  font-size: 0.5rem;
+  color: #6e7e9b;
+  text-decoration: none;
+}
+
+.beian a:hover {
+  color: #8192b1;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import avatar from "@/assets/images/avatar.png";
+</script>
+
+<template>
+  <main class="main">
+    <div class="container">
+      <h1 class="subject">星海昔织</h1>
+      <img :src="avatar" class="avatar" alt="" />
+      <p class="mission">分享IT思维，学习经验交流</p>
+      <p class="soon">comming soon...</p>
+    </div>
+  </main>
+</template>
+
+<style>
+.main {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  gap: 20px;
+}
+
+.subject {
+  font-size: 5rem;
+  font-weight: 400;
+  color: rgb(118, 184, 250);
+  display: flex;
+  justify-content: space-around;
+  padding: 0;
+  margin: 0;
+  background: linear-gradient(218.9deg, #a2facf 20.19%, #64acff 79.81%);
+  -webkit-background-clip: text;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+.avatar {
+  width: 100px;
+  height: 100px;
+  border-radius: 100%;
+}
+
+.mission {
+  color: #8192b1;
+  font-size: 1.1rem;
+}
+
+.soon {
+  color: #6e7e9b;
+}
+</style>
